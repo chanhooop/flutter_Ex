b438267357579fedcaa980e8b8a53ff47cd775b4
chahooo <cksghwwkd@gmail.com>
2022-02-16 16:15:31 +0900

    hiplants

--- chat_demo/lib/main.dart
+++ chat_demo/lib/main.dart
@@ -2,10 +2,9 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() async {
+void main() {
   // Firebase initialize 해주기 비동기로
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp;
+  // await Firebase.initializeApp;
   runApp(const MyApp());
 }
 
